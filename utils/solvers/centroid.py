34a622ccf2bb9065ab73b04758722ec8771a3817
mrityunjai01 <mrityunjaisingh3333@gmail.com>
2022-01-25 21:29:44 +0530

    centroid uses numpy

--- utils/solvers/centroid.py
+++ utils/solvers/centroid.py
@@ -3,7 +3,5 @@ from random import seed
 np.random.seed(1)
 seed(1)
 def centroid(points):
-    c = np.zeros(points.shape[1:])
-    for i in range(points.shape[0]):
-        c = c + points[i]
+    c = np.sum(points, axis=0)
     return c/points.shape[0]
