cf0458a2a83a34a4d20d6f4d955b4db828df880f
Mrityunjai Singh <mrityunjaisingh3333@gmail.com>
2022-02-24 12:17:51 +0530

    updated loader

--- utils/loader.py
+++ utils/loader.py
@@ -7,15 +7,16 @@ import mnist
 from random import seed
 np.random.seed(1)
 seed(1)
-def bin_mnist(class1 = 3, class2 = 8, totalsamp = None):
+def bin_mnist(class1 = 3, class2 = 8, totalsamp = 200):
     XALL = mnist.train_images()[:totalsamp]/255
     yALL = mnist.train_labels().reshape(-1,1)[:totalsamp].astype(np.int8)
-    yn = (yALL == class1)
-    yp = (yALL == class2)
-    ytot = (yn + yp).reshape(-1)
-    Xtrain = XALL[ytot]
-    ytrain = np.where(yALL[ytot] == class1, -1, 1).reshape(-1,1)
-    return Xtrain, ytrain
+    #yn = (yALL == class1)
+    #yp = (yALL == class2)
+    #ytot = (yn + yp).reshape(-1)
+    #Xtrain = XALL[ytot]
+    #ytrain = np.where(yALL[ytot] == class1, -1, 1).reshape(-1,1)
+    #return Xtrain, ytrain
+    return XALL,yALL
 
 def bin_CIFAR(class1 = 3, class2 = 8, totalsamp = None):
     transform = transforms.Compose([transforms.ToTensor(),])
@@ -23,12 +24,13 @@ def bin_CIFAR(class1 = 3, class2 = 8, totalsamp = None):
     train_loader = DataLoader(train_dataset, batch_size=len(train_dataset))
     XALL = next(iter(train_loader))[0].numpy().transpose((0,2,3,1))[:totalsamp]
     yALL = next(iter(train_loader))[1].numpy().reshape((-1,1))[:totalsamp]    
-    yn = (yALL == class1)
-    yp = (yALL == class2)
-    ytot = (yn + yp).reshape(-1)
-    Xtrain = XALL[ytot]
-    ytrain = np.where(yALL[ytot] == class1, -1, 1).reshape(-1,1)
-    return Xtrain, ytrain
+    #yn = (yALL == class1)
+    #yp = (yALL == class2)
+    #ytot = (yn + yp).reshape(-1)
+    #Xtrain = XALL[ytot]
+    #ytrain = np.where(yALL[ytot] == class1, -1, 1).reshape(-1,1)
+    #return Xtrain, ytrain
+    return XALL, yALL
 
 def normer(Xtrain):
     scaler = StandardScaler()
